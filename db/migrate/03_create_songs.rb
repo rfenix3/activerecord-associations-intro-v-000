2df36413a2b7737d104557c7f086337ffcf0bf98
Richard Fenix <richard.fenix@gmail.com>
2018-07-27 18:16:07 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,6 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change
-    t.string :name 
+    create_table :songs do |t|
+      t.string :name 
       t.integer :artist_id
       t.integer :genre_id
     end
